41fee7354172b6ec16df0675d11d06a01cf10bc0
Richmond Nkansah <richmondnkansah@gmail.com>
2022-10-15 15:13:43 -0400

    commit tree 1f99ea6012bde45b2082d15aacdc09f35a583daf

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+
+document.addEventListener('DOMContentLoaded', (e) =>{
+    e.preventDefault();
+   const paragrapgElement = document.getElementById("text")
+   paragrapgElement.textContent = "This is really cool!";
+})
